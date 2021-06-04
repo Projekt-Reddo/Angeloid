14c4a76338d677d3271c36f937522d12754b0dc1
Guy Handsome <sakakiyuya1412@gmail.com>
2021-06-04 23:15:08 +0700

    Thinh: update UserController

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -18,14 +18,38 @@ namespace Angeloid.Controllers
     [Route("api/user")]
     public class UserController : ControllerBase
     {
+        // List all users
+        [HttpGet]
+        [Route("")]
+        public async Task<ActionResult<List<User>>> ListAllUser([FromServices] Context context)
+        {
+            // Allow cors
+            Response.Headers.Add("Access-Control-Allow-Origin","*");
+
+            var users = await context.Users.ToListAsync();
+            if (users == null) {return NotFound();}
+            return users;
+        }
 
         //Insert new user (register)
         [HttpPost]
         [Route("")]
-        public async Task<ActionResult<Anime>> InsertUser([FromServices] Context context, [FromBody] Anime inputAnime)
+        public async Task<ActionResult<User>> InsertUser([FromServices] Context context, [FromBody] User user)
         {
+            // Allow cors
+            Response.Headers.Add("Access-Control-Allow-Origin","*");
 
-            return Ok("Insert done.");
+            if(!ModelState.IsValid) {return BadRequest(ModelState);}
+            // Check existed Username
+            var ExistUser = await context.Users.FirstOrDefaultAsync(u => u.UserName == user.UserName);
+            if (ExistUser != null) { return BadRequest("User name already existed");}
+
+            // var FacebookUser = await context.Users.FirstOrDefaultAsync(u => u.FacebookId == user.FacebookId);
+            // if (FacebookUser != null) { return BadRequest("Facebook Login");}
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+            return Ok("Register done.");
         }
 
         //Update user profile
@@ -34,6 +58,9 @@ namespace Angeloid.Controllers
         public async Task<ActionResult<Anime>> UpdateUser([FromServices] Context context, [FromBody] User user, int userId)
         {
 
+            // Allow cors
+            Response.Headers.Add("Access-Control-Allow-Origin","*");
+            
             return Ok("Update done.");
         }
     }
