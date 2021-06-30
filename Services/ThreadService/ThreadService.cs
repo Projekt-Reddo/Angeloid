020d5fa285d6757cb2508b2b7d2c8bcf1aefe747
Bao Loc Pham <phambaoloc163@gmail.com>
2021-06-30 20:57:19 +0700

    BaoLoc: change ListThreadFirst (startup) function in ThreadService

--- Services/ThreadService/ThreadService.cs
+++ Services/ThreadService/ThreadService.cs
@@ -65,10 +65,22 @@ namespace Angeloid.Services
         }
         public async Task<List<Thread>> ListThreadFirst()
         {
-            var threads = await _context.Threads
-                                            .OrderByDescending(t => t.ThreadId).Include(t => t.User)
-                                            .Take(10)
-                                            .ToListAsync();
+            var threads = await(
+                from t in _context.Threads
+                orderby t.ThreadId descending
+                select new Thread{
+                    ThreadId = t.ThreadId,
+                    Title = t.Title,
+                    Content = t.Content,
+                    Image = t.Image,
+                    UserId = t.UserId,
+                    User = new User{
+                        UserId = t.UserId,
+                        UserName = t.User.UserName,
+                        Avatar = t.User.Avatar
+                    }
+                }
+            ).Take(10).ToListAsync();
             return threads;
         }
         public async Task<List<Thread>> LoadMore(int loadId)
